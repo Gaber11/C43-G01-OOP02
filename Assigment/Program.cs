2fba7b2dced51c289a9e033f4fd34062739df217
Gaber11 <gaberezzat0@gmail.com>
2024-12-19 01:10:43 +0200

    Part 01.Try all what we have learned in the lecture.

---
+++ Assigment/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Assigment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region  Part 01.Try all what we have learned in the lecture.
+
+            #endregion
+            //Console.WriteLine("Hello, World!");
+            ////demo
+            ////demo
+        }
+    }
+}
