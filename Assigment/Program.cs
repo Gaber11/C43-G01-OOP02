dfbe6fa1ce78a9b16e7818ad3e6248e204fec4d1
Gaber11 <gaberezzat0@gmail.com>
2024-12-19 01:19:39 +0200

    part2) 1 Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.

--- Assigment/Program.cs
+++ Assigment/Program.cs
@@ -5,11 +5,27 @@
         static void Main(string[] args)
         {
             #region  Part 01.Try all what we have learned in the lecture.
-
-            #endregion
             //Console.WriteLine("Hello, World!");
             ////demo
             ////demo
+            #endregion
+
+            #region Part2) 1 Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+
+            //Person[] person = new Person[4];
+            //person[0] = new Person("Gaber", 24);
+            //person[1] = new Person("Ahmed", 22);
+            //person[2] = new Person("Hassan", 23);
+            //person[3] = new Person("Abdo", 19);
+            //for (int i = 0; i < person.Length; i++)
+            //{
+            //    Console.WriteLine($"Name: {person[i].Name}, Age: {person[i].Age}");
+            //}
+
+
+
+
+            #endregion
         }
     }
 }
