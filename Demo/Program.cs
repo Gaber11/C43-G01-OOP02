d97dc2c397a9cc82c9a5b6fb83a67b1b22e2ef60
Gaber11 <gaberezzat0@gmail.com>
2024-12-18 11:09:01 +0200

    Part 01 OOP Pillars - Encapsulation

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,9 +4,24 @@
     {
         static void Main(string[] args)
         {
-            
-            
-            
+            #region Part 01 OOP Pillars - Encapsulation
+
+          //    Employee employee=new Employee(10,"Gaber Ezzat" , 3000 );
+          // employee.Id = 30; //Set id directly through the attribute
+
+          ////  Console.WriteLine(employee.Id);
+          // // Console.WriteLine(employee);
+
+          //  employee.SetName("Mohamed Ali"); //Set Name with setter function
+
+          // // Console.WriteLine(employee.GetName());
+          //    employee.Salary = 90000;
+
+          //  Console.WriteLine(employee);
+
+            #endregion
+
+
 
         }
     }
